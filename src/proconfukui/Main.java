c1effd0c7f7d8270b2c1b2f7b8f4e67111e3d452
うたうたげ <kotalegoblock@gmail.com>
2025-06-27 10:01:49 +0900

    回転の実装

--- src/proconfukui/Main.java
+++ src/proconfukui/Main.java
@@ -6,32 +6,71 @@ import java.io.IOException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class Main {
-	static final String path = "/Users/utautage/problem.json"; // Generatorで生成したJSONのパス
-	public static void main(String[] args) {
+class Field {
+	int size;
+	int[][] entities;
+	private String format;
+	
+	Field(String path) {
+		JsonNode problem = null;
 		try {
-			JsonNode problem = new ObjectMapper().readTree(new File(path));
-			int size = problem.get("problem").get("field").get("size").asInt();
-			int[][] field = new int[size][size];
-			for (int y = 0; y < size; y++) {
-				for (int x = 0; x < size; x++) {
-					field[y][x] = problem.get("problem").get("field").get("entities").get(y).get(x).asInt();
-				}
-			}
-			showField(field, 0);
+			problem = new ObjectMapper().readTree(new File(path));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		size = problem.get("problem").get("field").get("size").asInt();
+		entities = new int[size][size];
+		for (int y = 0; y < size; y++) {
+			for (int x = 0; x < size; x++) {
+				entities[y][x] = problem.get("problem").get("field").get("entities").get(y).get(x).asInt();
+			}
+		}
+		format = "%" + String.valueOf(size * size / 2).length() + "d ";
 	}
 	
-	static void showField(int[][] field, int moveCount) {
-		System.out.println("手数:" + moveCount);
-		for (int y = 0; y < field.length; y++) {
-			for (int x = 0; x < field[0].length; x++) {
-				System.out.print(field[y][x] + " ");
+//	Field(Field from) {
+//		size = from.size;
+//		for (int y = 0; y < size; y++) {
+//			for (int x = 0; x < size; x++) {
+//				entities[y][x] = from.entities[y][x];
+//			}
+//		}
+//	}
+	
+	void print() {
+		for (int y = 0; y < size; y++) {
+			for (int x = 0; x < size; x++) {
+				System.out.printf(format, entities[y][x]);
 			}
 			System.out.println();
 		}
 		System.out.println();
+	}
+	
+	void rotate(int x, int y, int n) {
+		if (n < 2 || x < 0 || x + n > size || y < 0 || y + n > size) {
+			System.err.printf("導きが無効です(x: %d, y: %d, n: %d)\n", x, y, n);
+			return;
+		}
+		int[][] memo = new int[n][n];
+		for(int dy = 0; dy < n; dy++) {
+			for(int dx = 0; dx < n; dx++) {
+				memo[dy][dx] = entities[y + n - 1 - dx][x + dy];
+			}
+		}
+		for(int dy = 0; dy < n; dy++) {
+			for(int dx = 0; dx < n; dx++) {
+				entities[y + dy][x + dx] = memo[dy][dx];
+			}
+		}
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Field field = new Field("/Users/utautage/problem.json"); // Generatorで生成したJSONのパス
+		field.print();
+		field.rotate(0, 0, field.size);
+		field.print();
 	}
 }
